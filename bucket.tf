c88e91eac18ff9ba34974007e856bddaca8849b2
Himabindu1785059 <84077619+Himabindu1785059@users.noreply.github.com>
2021-05-29 21:38:03 +0530

    new

---
+++ bucket.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "storage" {
+
+  bucket = "srikanthbucket"
+
+  acl    = "public-read"
+
+  policy = file("policy.json")
+
+  website {
+
+    index_document = "index.html"
+
+    error_document = "error.html"
+  }
+
+
+
+  tags = {
+
+    Name        = "My bucket"
+
+    Environment = "Dev"
+
+  }
+
+}
+
+resource "aws_s3_bucket_object" "object1" {
+
+  bucket       = aws_s3_bucket.b1.id
+
+  key          = "index.html"
+
+  acl          = "public-read-write"
